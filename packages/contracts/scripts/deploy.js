e8854b0292c6cc5b127d55bb7cf470d87c098e0c
Chih Cheng Liang <chihchengliang@gmail.com>
2020-02-07 18:09:53 +0800

    deployment complete

---
+++ packages/contracts/scripts/deploy.js
@@ -0,0 +1,48 @@
+const bre = require('@nomiclabs/buidler')
+
+const mimcGenContract = require('circomlib/src/mimcsponge_gencontract.js')
+
+const MIMC_SEED = 'mimcsponge'
+const semaphoreTreeDepth = 12
+
+function buildMimcBytecode () {
+  return mimcGenContract.createCode(MIMC_SEED, 220)
+}
+
+async function deployContracts () {
+  await bre.run('compile')
+
+  const MiMC = bre.artifacts.require('MiMC')
+  const Semaphore = bre.artifacts.require('Semaphore')
+  const ProofOfBurn = bre.artifacts.require('ProofOfBurn')
+
+  MiMC.bytecode = buildMimcBytecode()
+
+  const mimcInstance = await MiMC.new()
+  await Semaphore.link(mimcInstance)
+  const semaphoreInstance = await Semaphore.new(semaphoreTreeDepth, 0, 0)
+  const proofOfBurnInstance = await ProofOfBurn.new(semaphoreInstance.address)
+
+  console.log('MiMC address', mimcInstance.address)
+  console.log('Semaphore address', semaphoreInstance.address)
+  console.log('ProofOfBurn address', proofOfBurnInstance.address)
+
+  return {
+    MiMC: mimcInstance,
+    Semaphore: semaphoreInstance,
+    ProofOfBurn: proofOfBurnInstance
+  }
+}
+
+async function main () {
+  await deployContracts()
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
