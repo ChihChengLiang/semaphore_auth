44d24759f604420603a5ef19a24e16abae7d35ab
Chih Cheng Liang <chihchengliang@gmail.com>
2020-02-18 06:13:58 +0800

    setup parcel

---
+++ packages/frontend/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import bulma from 'bulma'
+
+const App = () => {
+  return (
+      <div className='section'>
+        <div className='container'>
+            <h1>Foooo</h1>
+        </div>
+      </div>
+  )
+}
+
+const root = document.getElementById('root')
+
+ReactDOM.render(<App />, root)
