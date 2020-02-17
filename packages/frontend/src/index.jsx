23568742a73b4af20a270c4fac0b3ad119d3d93e
Chih Cheng Liang <chihchengliang@gmail.com>
2020-02-18 07:19:15 +0800

    add some fake posts

--- packages/frontend/src/index.jsx
+++ packages/frontend/src/index.jsx
@@ -2,13 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import bulma from 'bulma'
 
-const App = () => {
+const post = {
+  content:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. A mene tu? Qui potest igitur habitare in beata vita summi mali metus? Nihil opus est exemplis hoc facere longius. Sint ista Graecorum; Neque enim disputari sine reprehensione nec cum iracundia aut pertinacia recte disputari potest. Duo Reges: constructio interrete. Negare non possum.'
+}
+const posts = [post, post, post, post, post]
+
+const Posts = props => {
+  const posts = props.posts.map(post => <Post post={post} />)
+  return <div>{posts}</div>
+}
+
+const Post = props => {
   return (
-      <div className='section'>
-        <div className='container'>
-            <h1>Foooo</h1>
-        </div>
+    <div className='card'>
+      <div className='card-content'>
+        <div className='content'>{props.post.content}</div>
       </div>
+    </div>
+  )
+}
+
+const App = () => {
+  const p = posts.map((post, index) => {
+    ;<p>{post.content}</p>
+  })
+  return (
+    <div className='section'>
+      <div className='container'>
+        <h1>Foooo</h1>
+        <Posts posts={posts} />
+      </div>
+    </div>
   )
 }
 
