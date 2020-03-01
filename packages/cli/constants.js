75611d7d6464db3720ba99f5e47f2cb17471bb5e
Chih Cheng Liang <chihchengliang@gmail.com>
2020-03-02 05:41:01 +0800

    setup done

--- packages/cli/constants.js
+++ packages/cli/constants.js
@@ -6,6 +6,7 @@ const packageJson = require('./package.json')
 const paths = envPaths(packageJson.name)
 
 module.exports = {
+  ROOT_DIR: paths.data,
   CIRCUIT_PATH: path.join(paths.data, 'circuit.json'),
   PROVING_KEY_PATH: path.join(paths.data, 'proving_key.bin')
 }
--- packages/cli/setup.js
+++ packages/cli/setup.js
@@ -1,11 +1,42 @@
-const { CIRCUIT_PATH, PROVING_KEY_PATH } = require('./constants')
+const { ROOT_DIR, CIRCUIT_PATH, PROVING_KEY_PATH } = require('./constants')
 const {
   CIRCUIT_URL,
   PROVING_KEY_URL
 } = require('semaphore-auth-contracts/constants')
+const fs = require('fs')
 
-const setupHandler = argv => {
+const ora = require('ora')
+
+const fetch = require('node-fetch')
+
+const download = async (fromURL, toPath) => {
+  return fetch(fromURL).then(res => {
+    const dest = fs.createWriteStream(toPath)
+    res.body.pipe(dest)
+    return new Promise(function (resolve, reject) {
+      res.body.on('end', resolve)
+      res.body.on('error', reject)
+    })
+  })
+}
+
+const checkMaybeDownload = async (name, fromURL, toPath) => {
+  if (fs.existsSync(toPath)) {
+    console.info(`${name} exists, no need to download`)
+  } else {
+    const spinner = ora(`Downloading ${name}`).start()
+
+    await download(fromURL, toPath)
+
+    spinner.succeed(`Downloaded ${name} to ${toPath}`)
+  }
+}
+
+const setupHandler = async argv => {
   console.log('Setting up')
+  if (!fs.existsSync(ROOT_DIR)) fs.mkdirSync(ROOT_DIR)
+  await checkMaybeDownload('Circuit', CIRCUIT_URL, CIRCUIT_PATH)
+  await checkMaybeDownload('Proving key', PROVING_KEY_URL, PROVING_KEY_PATH)
 }
 
 module.exports = { setupHandler }
