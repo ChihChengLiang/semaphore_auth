423b6cec9a8bd6d00021b74d33e918f0433faca7
Chih Cheng Liang <chihchengliang@gmail.com>
2020-03-11 01:25:13 +0800

    update circuits and proving key urls

--- packages/contracts/constants.js
+++ packages/contracts/constants.js
@@ -5,13 +5,13 @@ module.exports = {
   MIMC_SEED: 'mimcsponge',
 
   // The circuits, proving key, verification key, and semaphore tree depth are interdependent
-  SEMAPHORE_TREE_DEPTH: 12, // This number depends on the circuit we use.
-  CIRCUIT_URL: 'https://oneofus.blob.core.windows.net/snarks/circuit.json',
+  SEMAPHORE_TREE_DEPTH: 20, // This number depends on the circuit we use.
+  CIRCUIT_URL: 'https://www.dropbox.com/s/3gzxjibqgb6ke13/circuit.json?dl=1',
   CIRCUIT_CACHE_PATH: path.join(__dirname, './cache/circuit.json'),
   PROVING_KEY_URL:
-    'https://oneofus.blob.core.windows.net/snarks/proving_key.bin',
+    'https://www.dropbox.com/s/qjlu6v125g7jkcq/proving_key.bin?dl=1',
   PROVING_KEY_CACHE_PATH: path.join(__dirname, './cache/proving_key.bin'),
   // verification_key.json is downloaded from
-  // https://oneofus.blob.core.windows.net/snarks/verification_key.json
+  // https://www.dropbox.com/s/rwjwu31c7pzhsth/verification_key.json?dl=1
   VERIFYING_KEY_PATH: path.join(__dirname, './assets/verification_key.json')
 }
