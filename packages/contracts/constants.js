9d50fbe52e514bd953a9971891696074e3f9f5b3
Chih Cheng Liang <chihchengliang@gmail.com>
2020-03-18 02:15:32 +0800

    use download link that has no cors issue

--- packages/contracts/constants.js
+++ packages/contracts/constants.js
@@ -6,12 +6,13 @@ module.exports = {
 
   // The circuits, proving key, verification key, and semaphore tree depth are interdependent
   SEMAPHORE_TREE_DEPTH: 20, // This number depends on the circuit we use.
-  CIRCUIT_URL: 'https://www.dropbox.com/s/3gzxjibqgb6ke13/circuit.json?dl=1',
+  CIRCUIT_URL:
+    'https://dl.dropboxusercontent.com/s/3gzxjibqgb6ke13/circuit.json?dl=1',
   CIRCUIT_CACHE_PATH: path.join(__dirname, './cache/circuit.json'),
   PROVING_KEY_URL:
-    'https://www.dropbox.com/s/qjlu6v125g7jkcq/proving_key.bin?dl=1',
+    'https://dl.dropboxusercontent.com/s/qjlu6v125g7jkcq/proving_key.bin?dl=1',
   PROVING_KEY_CACHE_PATH: path.join(__dirname, './cache/proving_key.bin'),
   // verification_key.json is downloaded from
-  // https://www.dropbox.com/s/rwjwu31c7pzhsth/verification_key.json?dl=1
+  // https://dl.dropboxusercontent.com/s/rwjwu31c7pzhsth/verification_key.json?dl=1
   VERIFYING_KEY_PATH: path.join(__dirname, './assets/verification_key.json')
 }
