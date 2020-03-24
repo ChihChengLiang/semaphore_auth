0ca3fecfcbfad99a15a16d9dbf7fedc8ef58cae7
Chih Cheng Liang <chihchengliang@gmail.com>
2020-03-24 16:47:15 +0800

    cosmetic pob contract view

--- packages/frontend/src/components/contracts.jsx
+++ packages/frontend/src/components/contracts.jsx
@@ -6,19 +6,27 @@ const ProofOfBurn = () => {
   const data = useProofOfBurnData()
 
   return data.isLoaded ? (
-    <div className='media'>
-      <div className='media-content'>
-        <p>
-          Address: <small>{data.address}</small>
-        </p>
-        <p>
-          Registration Fee:{' '}
-          <strong>{ethers.utils.formatEther(data.registrationFee)} ETH</strong>
-        </p>
-        <p>
-          <strong>{data.commitments}</strong> Members in the group
-        </p>
+    <div className='box has-background-light'>
+      <div className='content'>
+        <strong>Group managed by contract</strong> <small>{data.address}</small>
       </div>
+
+      <nav className='level'>
+        <div className='level-item has-text-centered'>
+          <div>
+            <p className='heading'>Members in the group</p>
+            <p className='title is-4'>{data.commitments}</p>
+          </div>
+        </div>
+        <div className='level-item has-text-centered'>
+          <div>
+            <p className='heading'>Registration Fee</p>
+            <p className='title is-4'>
+              {ethers.utils.formatEther(data.registrationFee)} ETH
+            </p>
+          </div>
+        </div>
+      </nav>
     </div>
   ) : (
     <p>Waiting contract data...</p>
