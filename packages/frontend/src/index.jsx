c17ce3e404792e36ba23712159dc34af55323478
jeonjonghyeok <tongher1685@gmail.com>
2021-09-04 00:51:38 +0900

    ignore frontend index.jsx github module

--- packages/frontend/src/index.jsx
+++ packages/frontend/src/index.jsx
@@ -14,7 +14,7 @@ import { ToastProvider } from 'react-toast-notifications'
 import Group from './pages/group'
 import About from './pages/about'
 
-import GithubCorner from './components/github.html'
+//import GithubCorner from './components/github.html'
 
 const links = [
   { path: '/about', title: 'About', component: <About /> },
@@ -58,7 +58,6 @@ const Layout = ({ children }) => (
         <div className='box'>{children}</div>
       </div>
     </div>
-    <div dangerouslySetInnerHTML={{ __html: GithubCorner }} />
   </section>
 )
 
