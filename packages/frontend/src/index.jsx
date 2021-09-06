97aaed3e45c5dd4503de2d2ab054e19623f6db9b
전종혁 <47622475+jeonjonghyeok@users.noreply.github.com>
2021-09-06 18:34:03 +0900

    Update packages/frontend/src/index.jsx
    Co-authored-by: Chih Cheng Liang <chihchengliang@gmail.com>

--- packages/frontend/src/index.jsx
+++ packages/frontend/src/index.jsx
@@ -14,7 +14,6 @@ import { ToastProvider } from 'react-toast-notifications'
 import Group from './pages/group'
 import About from './pages/about'
 
-//import GithubCorner from './components/github.html'
 
 const links = [
   { path: '/about', title: 'About', component: <About /> },
